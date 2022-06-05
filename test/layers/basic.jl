95740aca21465c91f6db8e1fa07917548a154f67
Carlo Lucibello <carlo.lucibello@gmail.com>
2022-06-05 10:33:19 +0200

    add tests

--- test/layers/basic.jl
+++ test/layers/basic.jl
@@ -64,5 +64,15 @@
         wg = WithGraph(model, g, traingraph=true)
         @test length(Flux.params(wg)) == length(Flux.params(model)) + length(Flux.params(g))
     end
+
+    @testset "GNNGraph array input" begin
+        gs = [rand_graph(5, 6, ndata=rand(2, 5), graph_type=GRAPH_T) for _ in 1:4]
+        l = GCNConv(2 => 3)
+        y = l(gs, rand(2, 20))
+        @test size(y) == (3, 20)
+
+        gout = l(gs)
+        @test size(gout.ndata.x) == (3, 20)
+    end
 end
 
